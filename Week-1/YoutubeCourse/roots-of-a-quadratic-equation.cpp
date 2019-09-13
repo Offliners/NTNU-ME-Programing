43778b892b9e25ff2c3c6ad03fd77bdd40decc1e
Offliner <42777325+Offliners@users.noreply.github.com>
2019-09-13 23:08:26 +0800

    Create roots-of-a-quadratic-equation.cpp

---
+++ Week-1/YoutubeCourse/roots-of-a-quadratic-equation.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main()
+{
+	double a, b, c, x1, x2, t;
+	printf_s("請依序輸入a,b,c之值，兩數之間以空格隔開 :");
+	scanf_s("%lf %lf %lf", &a, &b, &c);
+	printf_s("a = %f  b = %f  c = %f\n", a, b, c);
+	t = sqrt(b * b - 4 * a * c);
+	x1 = (-b + t) / (2 * a);
+	x2 = (-b - t) / (2 * a);
+	printf_s("x1 = %f x2 = %f\n", x1, x2);
+	system("pause");
+}
