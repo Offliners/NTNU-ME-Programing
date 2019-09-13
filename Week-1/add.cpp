f3e8122b472086b2693765b1e518daf2d044b03a
Offliner <42777325+Offliners@users.noreply.github.com>
2019-09-13 22:47:09 +0800

    Update add.cpp

--- Week-1/add.cpp
+++ Week-1/add.cpp
@@ -4,9 +4,11 @@
 int main()
 {
 	int x, y, z;
+	printf_s("Please enter x : ");
 	scanf_s("%d", &x);
+	printf_s("Please enter y : ");
 	scanf_s("%d", &y);
 	z = x + y;
-	printf_s("%d\n", z);
+	printf_s("x + y = %d\n", z);
 	system("pause");
 }
