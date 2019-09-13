25f006fa151d66381e4a1ab4996f599e2768a039
Offliner <42777325+Offliners@users.noreply.github.com>
2019-09-13 23:32:50 +0800

    Create Classwork1-1.cpp

---
+++ Week-1/Classwork/Classwork1-1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int x = 5;
+	double y = 0.031415;
+	char z = 'a';
+	printf_s("x=%d\n", x);
+	printf_s("y=%f\n", y);
+	printf_s("y=%e\n", y);
+	printf_s("z=%c\n", z);
+	system("pause");
+}
