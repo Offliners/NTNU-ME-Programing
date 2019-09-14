cba090ccf8c9b2180cfab09131afc74c33d32e42
Offliner <42777325+Offliners@users.noreply.github.com>
2019-09-14 22:17:03 +0800

    Create discount.cpp

---
+++ Week-2/YoutubeCourse/discount.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	char membership;
+	double discount, total;
+	printf_s("請輸入會員等級 : ");
+	scanf_s("%c", &membership,sizeof(membership));
+	printf_s("請輸入消費金額 : ");
+	scanf_s("%lf", &total);
+	switch (membership)
+	{
+		case 'A':
+			discount = 0.8;
+			break;
+		case 'B':
+			discount = 0.9;
+			break;
+		default:
+			discount = 1;
+			break;
+	}
+	total *= discount;
+	printf_s("折扣後的消費金額 = %f\n", total);
+	system("pause");
+}
