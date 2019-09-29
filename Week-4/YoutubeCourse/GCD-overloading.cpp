a7db584cf48b90543f1284723c90daf8cb412149
Offliner <42777325+Offliners@users.noreply.github.com>
2019-09-30 00:15:29 +0800

    Create GCD-overloading.cpp

---
+++ Week-4/YoutubeCourse/GCD-overloading.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int GCD(int num1, int num2);
+int GCD(int num1, int num2, int num3);
+
+int main()
+{
+	int a, b, c, result;
+	printf_s("Enter three integers : ");
+	scanf_s("%d %d %d", &a, &b, &c);
+	result = GCD(a, b, c);
+	printf_s("(%d,%d,%d) = %d\n", a, b, c, result);
+	system("pause");
+}
+
+int GCD(int num1, int num2)
+{
+	int temp;
+	while (num1 % num2 != 0)
+	{
+		temp = num1 % num2;
+		num1 = num2;
+		num2 = temp;
+	}
+	return num2;
+}
+
+int GCD(int num1, int num2, int num3)
+{
+	return GCD(GCD(num1, num2), num3);
+}
