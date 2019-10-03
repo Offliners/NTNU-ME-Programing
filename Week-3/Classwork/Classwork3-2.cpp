67867ea13607db6c1b5504110561340d3e135827
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-04 02:00:48 +0800

    Update Classwork3-2.cpp

--- Week-3/Classwork/Classwork3-2.cpp
+++ Week-3/Classwork/Classwork3-2.cpp
@@ -1,1 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+	int i, j, inner, outer;
+	printf_s("=============列印星狀圖=============\n");
+	printf_s("請輸入外圈邊長及內圈邊長 : ");
+	scanf_s("%d %d", &outer, &inner);
+	if (outer % 2 != inner % 2)
+		printf_s("輸入錯誤\n");
+	else
+	{
+		for (i = 1; i <= outer; i++)
+		{
+			for (j = 1; j <= outer; j++)
+			{
+				if (((i <= (outer + inner) / 2) && (i > (outer - inner) / 2)) && ((j <= (outer + inner) / 2) && (j > (outer - inner) / 2)))
+					printf_s("  ");
+				else
+					printf_s("* ");
+			}
+			printf_s("\n");
+		}
+	}
+	system("pause");
+}
