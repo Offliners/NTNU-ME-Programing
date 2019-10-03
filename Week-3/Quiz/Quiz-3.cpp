6b4360ac7c16aaf3ca5aa0cced07b8b148d50b46
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-04 01:02:37 +0800

    Update Quiz-3.cpp

--- Week-3/Quiz/Quiz-3.cpp
+++ Week-3/Quiz/Quiz-3.cpp
@@ -1,1 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+	int num, i;
+	printf_s("請輸入一個整數 : ");
+	scanf_s("%d", &num);
+	printf_s("%d的因數有 L\n", num);
+	for (i = 1; i * i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			if (i == num / i)
+				printf_s("%8d\n", i);
+			else
+				printf_s("%8d,%8d\n", i, num / i);
+		}
+	}
+	system("pause");
+}
