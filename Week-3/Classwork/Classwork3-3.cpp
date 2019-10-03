35b6304e3d629f6d08f7ae47fae2715a3e3ed67d
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-04 01:42:28 +0800

    Update Classwork3-3.cpp

--- Week-3/Classwork/Classwork3-3.cpp
+++ Week-3/Classwork/Classwork3-3.cpp
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+	int i, j, length;
+	printf_s("===============列印圖形===============\n");
+	printf_s("Please input length : ");
+	scanf_s("%d", &length);
+	for (i = 1; i <= length; i++)
+	{
+		for (j = 1; j <= length; j++)
+		{
+			if ((i == j) || (i + j == length + 1))
+				printf_s("%c ", 'X');
+			else
+				printf_s("%c ", 'O');
+		}
+		printf_s("\n");
+	}
+	system("pause");
+}
