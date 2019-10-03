7d147d88e2a03dde5487d0494d4b43537561cd76
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-04 01:52:25 +0800

    Update Classwork3-4.cpp

--- Week-3/Classwork/Classwork3-4.cpp
+++ Week-3/Classwork/Classwork3-4.cpp
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+	int num, rem, rev = 0;
+	printf_s("輸入 : ");
+	scanf_s("%d", &num);
+	while (num != 0)
+	{
+		rem = num % 10;
+		num /= 10;
+		rev = rev * 10 + rem;
+	}
+	printf_s("輸出 : %d\n", rev);
+	system("pause");
+}
