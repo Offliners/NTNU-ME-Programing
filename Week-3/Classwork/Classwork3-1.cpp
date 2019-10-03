48f70b5fe65674dc9ac72e3b3523c8458f0d3d12
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-04 01:17:16 +0800

    Update Classwork3-1.cpp

--- Week-3/Classwork/Classwork3-1.cpp
+++ Week-3/Classwork/Classwork3-1.cpp
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int power(int x, int y);
+
+int main()
+{
+	int i, j;
+	for (i = 1; i <= 5; i++)
+	{
+		for (j = 1; j <= 5; j++)
+			printf_s("%4d^%d=%4d", j, i, power(j, i));
+		printf_s("\n");
+	}
+	system("pause");
+}
+
+int power(int x, int y)
+{
+	int t, sum = x;
+	for (t = 1; t < y; t++)
+		sum *= x;
+	return sum;
+}
