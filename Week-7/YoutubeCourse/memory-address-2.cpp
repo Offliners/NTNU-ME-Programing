0e3f037394ef52815b52d4c2b07e50998aff6bd1
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-13 16:01:01 +0800

    Update memory-address-2.cpp

--- Week-7/YoutubeCourse/memory-address-2.cpp
+++ Week-7/YoutubeCourse/memory-address-2.cpp
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+	char cx = 0;
+	int nx = 1;
+	float fx = 2;
+	double dx = 3;
+
+	printf_s("The address of cx : %p\n", &cx);
+	printf_s("The address of nx : %p\n", &nx);
+	printf_s("The address of fx : %p\n", &fx);
+	printf_s("The address of dx : %p\n", &dx);
+
+	printf_s("The size of cx : %d\n", sizeof(cx));
+	printf_s("The size of nx : %d\n", sizeof(nx));
+	printf_s("The size of fx : %d\n", sizeof(fx));
+	printf_s("The size of dx : %d\n", sizeof(dx));
+
+	system("pause");
+}
