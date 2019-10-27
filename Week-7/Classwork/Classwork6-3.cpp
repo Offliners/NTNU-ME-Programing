c13b4a8c623a2ea98799978ee9490cd2d0300e2d
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-27 23:42:23 +0800

    Update Classwork6-3.cpp

--- Week-7/Classwork/Classwork6-3.cpp
+++ Week-7/Classwork/Classwork6-3.cpp
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int reverse(int , int);
+
+int main()
+{
+	int num;
+
+	while (1)
+	{
+		printf_s("Enter a number : ");
+		if (scanf_s("%d", &num) == EOF)
+			break;
+		if (num < 0 || num > 999999999)
+			printf_s("輸入數字不合法!\n");
+		else
+			printf_s("The reserved number is %d\n", reverse(num, 0));
+	}
+	system("pause");
+}
+
+int reverse(int num, int rev)
+{
+	if (num == 0)
+		return rev;
+	else
+		return reverse(num / 10, rev * 10 + num % 10);
+}
