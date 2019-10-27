15bdd8b38a8a02a407a7b9069406374537145c75
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-28 00:02:54 +0800

    Update Classwork6-2.cpp

--- Week-7/Classwork/Classwork6-2.cpp
+++ Week-7/Classwork/Classwork6-2.cpp
@@ -1,1 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int sumofSeries(int , int , int );
+
+int main()
+{
+	int a, d, n, sum = 0;
+
+	printf_s("S=a+(a+d)+(a+2d)+(a+3d)+…….+(a+(n-1)d)\n");
+	printf_s("請輸入首項 : ");
+	scanf_s("%d", &a);
+	printf_s("請輸入公差 : ");
+	scanf_s("%d", &d);
+	printf_s("請輸入項數 : ");
+	scanf_s("%d", &n);
+	sum = sumofSeries(a, d, n);
+	printf_s("Sum = %d\n", sum);
+	system("pause");
+}
+
+int sumofSeries(int a, int d, int n)
+{
+	if (n == 0)
+		return 0;
+	else
+		return a + sumofSeries(a + d, d, n - 1);
+}
