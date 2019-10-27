5d85307161020db92e0d930a370f90c5f611e1ca
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-27 15:54:54 +0800

    Update Classwork4-4.cpp

--- Week-5/Classwork/Classwork4-4.cpp
+++ Week-5/Classwork/Classwork4-4.cpp
@@ -1,1 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+bool isPrime(int);
+int noOfPrime(int);
+
+int main()
+{
+	int num;
+
+	do
+	{
+		printf_s("輸入一整數<Ctrl z 結束程式> : ");
+		if (scanf_s("%d", &num) == EOF)
+			break;
+		printf_s("%d內共有%d個質數\n", num, noOfPrime(num));
+	} while (1);
+	system("pause");
+}
+
+bool isPrime(int num)
+{
+	for (int i = 2; i < num; i++)
+		if (num % i == 0)
+			return false;
+	return true;
+}
+
+int noOfPrime(int num)
+{
+	int i, count = 0;
+
+	for (i = 2; i <= num; i++)
+		if (isPrime(i))
+			count++;
+	return count;
+}
