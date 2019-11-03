305bdfd2df7d2dc46b5bbda663f28cce60c8338f
Offliner <42777325+Offliners@users.noreply.github.com>
2019-11-03 17:53:47 +0800

    Create midMain.cpp

---
+++ Week-8/PreviousMidExam/106MidExam/midMain.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "menu.h"
+#include "advance.h"
+#include "fundamental.h"
+
+int main()
+{
+	int exit;
+	while (1)
+	{
+		exit = 0;
+		switch (mainMenu())
+		{
+		case 0:
+			exit = 1;
+			break;
+		case 1:
+			Fundamental();
+			break;
+		case 2:
+			Advance();
+			break;
+		default:
+			printf_s("Input Error!\n");
+			system("pause");
+			break;
+		}
+		if (exit == 1)
+		{
+			printf_s("結束程式\n");
+			break;
+		}
+	}
+	system("pause");
+}
