2d7e29026f72c88107070fafafab4d561c1beed1
Offliner <42777325+Offliners@users.noreply.github.com>
2019-11-03 17:55:21 +0800

    Create Menu.cpp

---
+++ Week-8/PreviousMidExam/106MidExam/Menu.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mainMenu()
+{
+	int select;
+	system("cls");
+	printf_s("==========Menu for 106 MidExam==========\n");
+	printf_s("1. Fundamentals\n");
+	printf_s("2. Advance\n");
+	printf_s("0. Exit\n");
+	printf_s("Please give your selection :\n");
+	scanf_s("%d", &select);
+	return select;
+}
+
+int FundamentalMenu()
+{
+	int select;
+	system("cls");
+	printf_s("==========Menu for Fundamental Problems==========\n");
+	printf_s("1. P1\n");
+	printf_s("2. P2\n");
+	printf_s("3. P3\n");
+	printf_s("4. P4\n");
+	printf_s("0. Main Menu\n");
+	printf_s("Please give your selection :\n");
+	scanf_s("%d", &select);
+	return select;
+}
+
+int AdvanceMenu()
+{
+	int select;
+	system("cls");
+	printf_s("==========Menu for Advance Problems==========\n");
+	printf_s("1. P5\n");
+	printf_s("2. P6\n");
+	printf_s("0. Main Menu\n");
+	printf_s("Please give your selection :\n");
+	scanf_s("%d", &select);
+	return select;
+}
