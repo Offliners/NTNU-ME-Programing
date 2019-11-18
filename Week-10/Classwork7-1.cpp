047455b8c41ddd4c05035b846758dd527e40a436
Offliner <42777325+Offliners@users.noreply.github.com>
2019-11-18 11:03:18 +0800

    Create Classwork7-1.cpp

---
+++ Week-10/Classwork7-1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+double Sqrt(int);
+
+int main()
+{
+	int num;
+
+	printf_s("Please input a num : ");
+	scanf_s("%d", &num);
+	printf_s("sqrt(%d) = %f\n", num, Sqrt(num));
+	system("pause");
+}
+
+double Sqrt(int num)
+{
+	double n = 1;
+
+	while (fabs(n * n - num) >= 1e-6)
+		n = 0.5 * (n + num / n);
+	return n;
+}
